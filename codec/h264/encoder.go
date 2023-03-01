ef4b28579e84266e6511d8e3c60a9a14f69f2e6a
Clyde Bazile <clyde@viam.com>
2023-03-01 13:30:57 -0500

    use goav

--- codec/h264/encoder.go
+++ codec/h264/encoder.go
@@ -1,27 +1,58 @@
 package h264
 
+import (
+	"context"
+	"github.com/edaniels/golog"
+	"github.com/giorgisio/goav/avformat"
+	"github.com/pion/mediadevices/pkg/codec"
+	"image"
+)
+
 // ffmpeg -f v4l2 -i /dev/video0 -codec:v h264_v4l2m2m webcam.mkv
 //
 // Main options
 // -f fmt (input/output)
-// 		Force input or output file format. The format is normally auto-detected for input files and guessed from the file extension for output files, so this
-// 		option is not needed in most cases.
+//
+//	Force input or output file format. The format is normally auto-detected for input files and guessed from the file extension for output files, so this
+//	option is not needed in most cases.
+//
 // -i url (input)
-// 		input file url
+//
+//	input file url
+//
 // -c[:stream_specifier] codec (input/output,per-stream)
 // -codec[:stream_specifier] codec (input/output,per-stream)
-// 		Select an encoder (when used before an output file) or a decoder (when used before an input file) for one or more streams. codec is the name of a
-// 		decoder/encoder or a special value "copy" (output only) to indicate that the stream is not to be re-encoded.
 //
-// 		For example
+//	Select an encoder (when used before an output file) or a decoder (when used before an input file) for one or more streams. codec is the name of a
+//	decoder/encoder or a special value "copy" (output only) to indicate that the stream is not to be re-encoded.
 //
-// 				ffmpeg -i INPUT -map 0 -c:v libx264 -c:a copy OUTPUT
+//	For example
 //
-// 			encodes all video streams with libx264 and copies all audio streams.
+//			ffmpeg -i INPUT -map 0 -c:v libx264 -c:a copy OUTPUT
 //
-// 			For each stream, the last matching "c" option is applied, so
+//		encodes all video streams with libx264 and copies all audio streams.
 //
-// 				ffmpeg -i INPUT -map 0 -c copy -c:v:1 libx264 -c:a:137 libvorbis OUTPUT
+//		For each stream, the last matching "c" option is applied, so
 //
-// 			will copy all the streams except the second video, which will be encoded with libx264, and the 138th audio, which will be encoded with libvorbis.
+//			ffmpeg -i INPUT -map 0 -c copy -c:v:1 libx264 -c:a:137 libvorbis OUTPUT
 //
+//		will copy all the streams except the second video, which will be encoded with libx264, and the 138th audio, which will be encoded with libvorbis.
+type encoder struct {
+	codec  codec.ReadCloser
+	img    image.Image
+	logger golog.Logger
+}
+
+func (h *encoder) Read() (img image.Image, release func(), err error) {
+	return h.img, nil, nil
+}
+
+func (h *encoder) Encode(_ context.Context, img image.Image) ([]byte, error) {
+	_ = avformat.AvformatConfiguration()
+	h.img = img
+	data, release, err := h.codec.Read()
+	dataCopy := make([]byte, len(data))
+	copy(dataCopy, data)
+	release()
+	return dataCopy, err
+}
