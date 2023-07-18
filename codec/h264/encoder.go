e2cd100f6e9de005de670f0476d8f5181e045ef9
Clyde Bazile <clyde@viam.com>
2023-07-18 00:33:06 -0400

    updates

--- codec/h264/encoder.go
+++ codec/h264/encoder.go
@@ -9,7 +9,6 @@ import (
 	"github.com/edaniels/golog"
 	"github.com/giorgisio/goav/avcodec"
 	"github.com/giorgisio/goav/avutil"
-	"github.com/pion/mediadevices/pkg/io/video"
 	"github.com/pkg/errors"
 	ourcodec "github.com/viamrobotics/gostream/codec"
 	"image"
@@ -49,30 +48,25 @@ import (
 // INSTALLING FFMPEG FROM SOURCE:
 //
 //	use v4.X https://github.com/FFmpeg/FFmpeg/tree/release/4.4
+const CODEC = "h264_v4l2m2m"
+
 type encoder struct {
-	img     image.Image
 	codec   *avcodec.Codec
 	context *avcodec.Context
 	width   int
 	height  int
-	reader  video.Reader
 	pixFmt  int
 	// TODO: The resulting struct must be freed using av_frame_free().
-	frame *avutil.Frame
-	pts   int64
+	frame  *avutil.Frame
+	pts    int64
+	logger golog.Logger
 }
 
-func (h *encoder) Read() (img image.Image, release func(), err error) {
-	return h.img, nil, nil
-}
+func NewEncoder(width, height, keyFrameInterval int, logger golog.Logger) (ourcodec.VideoEncoder, error) {
+	h := &encoder{width: width, height: height, logger: logger}
 
-func NewEncoder(width, height, _ int, _ golog.Logger) (ourcodec.VideoEncoder, error) {
-	h := &encoder{width: width, height: height}
-
-	encName := "h264_v4l2m2m"
-	h.codec = avcodec.AvcodecFindEncoderByName(encName)
-	if h.codec == nil {
-		panic(fmt.Sprintf("cannot find encoder '%s'", encName))
+	if h.codec = avcodec.AvcodecFindEncoderByName(CODEC); h.codec == nil {
+		return nil, errors.Errorf("cannot find encoder '%s'", CODEC)
 	}
 
 	h.context = h.codec.AvcodecAllocContext3()
@@ -95,12 +89,10 @@ func NewEncoder(width, height, _ int, _ golog.Logger) (ourcodec.VideoEncoder, er
 	if h.frame = avutil.AvFrameAlloc(); h.frame == nil {
 		return nil, errors.New("cannot alloc frame")
 	}
-
-	h.reader = video.ToI420((video.ReaderFunc)(h.Read))
 	return h, nil
 }
 
-func (h *encoder) Encode(_ context.Context, img image.Image) ([]byte, error) {
+func (h *encoder) Encode(ctx context.Context, img image.Image) ([]byte, error) {
 	if err := avutil.AvSetFrame(h.frame, h.width, h.height, h.pixFmt); err != nil {
 		return nil, errors.Wrap(err, "cannot set frame")
 	}
@@ -109,21 +101,19 @@ func (h *encoder) Encode(_ context.Context, img image.Image) ([]byte, error) {
 		return nil, errors.Wrap(avutil.ErrorFromCode(ret), "cannot make frame writable")
 	}
 
-	h.img = img
-	yuvImg, _, err := h.reader.Read()
-
-	if err != nil {
-		return nil, errors.Wrap(err, "cannot get image")
-	}
-
-	h.frame.AvSetFrameFromImg(yuvImg.(*image.YCbCr))
+	h.frame.AvSetFrameFromImg(img.(*image.YCbCr))
 	h.frame.AvSetFramePTS(h.pts)
 	h.pts++
 
-	return h.encodeBytes()
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		return h.encodeBytes(ctx)
+	}
 }
 
-func (h *encoder) encodeBytes() ([]byte, error) {
+func (h *encoder) encodeBytes(ctx context.Context) ([]byte, error) {
 	pkt := avcodec.AvPacketAlloc()
 	if pkt == nil {
 		return nil, errors.Errorf("cannot allocate packet")
@@ -138,6 +128,12 @@ func (h *encoder) encodeBytes() ([]byte, error) {
 
 	var bytes []byte
 	for ret := 0; ret >= 0; {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+
 		ret = h.context.AvCodecReceivePacket(pkt)
 		if ret == avutil.AvErrorEOF || ret == avutil.AvErrorEAGAIN {
 			break
